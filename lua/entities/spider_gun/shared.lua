e43ea84b4d56584eb941abe5a0ea7aa27496a44f
TheKillerNL <gooferclan@gmail.com>
2015-05-30 08:48:40 +0200

    Fixed Bugs in Lua
    Lua Bug Fixed

---
+++ lua/entities/spider_gun/shared.lua
@@ -0,0 +1,9 @@
+ENT.Type = "anim"
+ENT.Base = "base_anim"
+ENT.PrintName		= "Spiderman's Swep"
+ENT.Author			= "DORPZ BEETCH"
+ENT.Information			= "Uses 9mm ammo"
+ENT.Category		= "Superhero"
+
+ENT.Spawnable		= true
+ENT.AdminSpawnable	= true
